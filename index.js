66e63fc7d718ac3cce26396d7af58ec4c4c771fb
harshilpatel05 <patelharshil2005@gmail.com>
2026-04-07 21:57:45 +0530

    fixed wrong path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { supabase } = require('./lib/supabase');
+const { supabase } = require('supabase');
 
 const app = express();
 app.use(express.json());
